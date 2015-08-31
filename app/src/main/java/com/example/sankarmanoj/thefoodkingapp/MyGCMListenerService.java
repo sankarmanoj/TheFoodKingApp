fa3d449dbd3855933442368a6f91d537a80202e0
sankarmanoj <me@sankar-manoj.com>
2015-08-31 20:37:17 +0530

    Added Notification for GCM Message

--- app/src/main/java/com/example/sankarmanoj/thefoodkingapp/MyGCMListenerService.java
+++ app/src/main/java/com/example/sankarmanoj/thefoodkingapp/MyGCMListenerService.java
@@ -1,9 +1,13 @@
 package com.example.sankarmanoj.thefoodkingapp;
 
-import android.app.Service;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -14,37 +18,36 @@ public class MyGCMListenerService extends GcmListenerService {
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
-        Log.i(TAG,data.toString());
-        String Stringtype= data.getString("type");
-        if (Stringtype.equals("button"))
-        {
+        Log.i(TAG, data.toString());
+        String Stringtype = data.getString("type");
+        if (Stringtype.equals("button")) {
 
-                String button = data.getString("button-type");
-                if (button.equals("confirm"))
-                {
-                    Log.i(TAG,"Sent Confirm Broadcast");
-                    Intent confirm = new Intent("order-status");
-                    confirm.putExtra("button","confirm");
-                    confirm.putExtra("status","Order Confirmed");
-                    LocalBroadcastManager.getInstance(this).sendBroadcast(confirm);
-                }
-                else  if (button.equals("dispatch"))
-                {
-                    Intent confirm = new Intent("order-status");
-                    confirm.putExtra("button","dispatch");
-                    confirm.putExtra("status","Order has been Dispatched.");
-                    LocalBroadcastManager.getInstance(this).sendBroadcast(confirm);
-                } if (button.equals("close"))
-                      {
-                        Intent orderclose = new Intent(getApplicationContext(),MainActivity.class);
-                          orderclose.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                          startActivity(orderclose);
-                    }
-
+            String button = data.getString("button-type");
+            if (button.equals("confirm")) {
+                Log.i(TAG, "Sent Confirm Broadcast");
+                Intent confirm = new Intent("order-status");
+                confirm.putExtra("button", "confirm");
+                confirm.putExtra("status", "Order Confirmed");
+                sendNotification("Order Confirmed");
+                LocalBroadcastManager.getInstance(this).sendBroadcast(confirm);
+            } else if (button.equals("dispatch")) {
+                Intent confirm = new Intent("order-status");
+                confirm.putExtra("button", "dispatch");
+                confirm.putExtra("status", "Order has been Dispatched.");
+                sendNotification("Order Dispatched");
+                LocalBroadcastManager.getInstance(this).sendBroadcast(confirm);
 
             }
+            if (button.equals("close")) {
+                Intent orderclose = new Intent(getApplicationContext(), MainActivity.class);
+                orderclose.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(orderclose);
+            }
 
-        Log.d(TAG, "From: " + from);
+
+        }
+    }
+
 
         /**
          * Production applications would usually process the message here.
@@ -52,6 +55,24 @@ public class MyGCMListenerService extends GcmListenerService {
          *     - Store message in local database.
          *     - Update UI.
          */
+    private void sendNotification(String message) {
+        final Uri RollURI = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.roll);
+        Intent intent = new Intent(this, Checkout.class);
+        intent.putExtra("action","get-status");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("FoodKing Order Update")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(RollURI)
+                .setColor(Color.argb(255,255,0,0))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager =   (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(2,notificationBuilder.build());
 
 
     }
